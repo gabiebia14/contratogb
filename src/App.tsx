789aa1457f3334ed1d33cc69469b778dba032505
gabiebia14 <160181364+gabiebia14@users.noreply.github.com>
2025-02-14 17:16:39 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Toaster } from '@/components/ui/sonner';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
